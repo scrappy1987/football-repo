46e41170172b0fec727a1f57ac0e9437aacefe26
scrappy1987 <jagordon87@msn.com>
2018-08-07 16:31:49 +0100

    greatest feature branch

--- src/main/java/com/qa/football/App.java
+++ src/main/java/com/qa/football/App.java
@@ -1,10 +1,12 @@
 package com.qa.football;
 
+import com.qa.football.team.Rangers;
+
 public class App {
 
 	public static void main(String[] args) {
-		System.out.println("My football application");
-
+		Rangers rangers = new Rangers("Best team ever");
+		System.out.println("This is the message: " + rangers.getMessage() );
 	}
 
 }
---
+++ src/main/java/com/qa/football/team/Rangers.java
@@ -0,0 +1,19 @@
+package com.qa.football.team;
+
+public class Rangers {
+	
+	private String message;
+	
+	public Rangers(String message) 
+	{
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+}
